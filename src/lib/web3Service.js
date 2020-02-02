d9e7c33fc11374a1e5dd7f3031ec79f430d78e74
ensbot <58172776+ensbot@users.noreply.github.com>
2020-02-02 20:56:13 +0000

    My commit message -4712-01-01

--- src/lib/web3Service.js
+++ src/lib/web3Service.js
@@ -21,7 +21,7 @@ export const getEthereumRegistrarAddress = (networkId = '1') => {
 export const getEthereumRegistryAddress = (networkId = '1') => {
   switch (networkId) {
     case '1':
-      return '0x314159265dD8dbb310642f98f50C066173C1259b';
+      return '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e';
     case '3':
       return '0x0';
     default:
